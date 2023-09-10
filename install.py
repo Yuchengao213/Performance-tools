7a925d14b59c8c4dc26be5ae18bcd1c78f8a7537
Yuchengao213 <122886889+Yuchengao213@users.noreply.github.com>
2023-09-10 23:28:30 +0200

    Update install.py

--- install.py
+++ install.py
@@ -1,11 +1,7 @@
 import importlib
 import subprocess
 
-<<<<<<< HEAD
 
-=======
-# 定义要检查的包列表
->>>>>>> 14c9b484b4111a5564278f2115ef3fe28fe44f53
 required_packages = [
     "pycuda",
     "pynvml",
@@ -17,18 +13,10 @@ required_packages = [
 def check_and_install_packages(packages):
     for package in packages:
         try:
-<<<<<<< HEAD
-=======
-            # 尝试导入包，如果导入失败会抛出ImportError异常
->>>>>>> 14c9b484b4111a5564278f2115ef3fe28fe44f53
             importlib.import_module(package)
             print(f"{package} is already installed.")
         except ImportError:
             print(f"{package} is not installed. Installing...")
-<<<<<<< HEAD
-=======
-            # 使用pip安装缺失的包
->>>>>>> 14c9b484b4111a5564278f2115ef3fe28fe44f53
             subprocess.run(["pip", "install", package])
             print(f"{package} has been successfully installed.")
 
